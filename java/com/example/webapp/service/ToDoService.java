b32bef20b22550db7364b7327e3eadc108f13a7b
NagisaHamaguchi <n.hamaguchi.turquoise@gmail.com>
2025-12-20 12:27:01 +0900

    Initial commit

---
+++ java/com/example/webapp/service/ToDoService.java
@@ -0,0 +1,24 @@
+package com.example.webapp.service;
+
+import java.util.List;
+
+import com.example.webapp.entity.ToDo;
+
+//  ToDoサービス
+public interface ToDoService {
+
+//  全「すること」を検索します
+  List<ToDo> findAllToDo();
+  
+//  指定されたIDの「すること」を検索します
+  ToDo findByIdToDo(Integer id);
+  
+//  「すること」を新規登録します
+  void insertToDo(ToDo toDo);
+  
+//  「すること」を更新します
+  void  updateTpDo(ToDo toDo);
+  
+//  指定されたIDの「すること」を削除します
+  void  deleteToDo(Integer id);
+}
